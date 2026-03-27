02ee4193317d284f19d8415141f244d31342f420
KyungMin_Kim <107176570+rvbear@users.noreply.github.com>
2026-03-28 01:00:13 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2946-matrix-similarity-after-cyclic-shifts/2946-matrix-similarity-after-cyclic-shifts.java
+++ 2946-matrix-similarity-after-cyclic-shifts/2946-matrix-similarity-after-cyclic-shifts.java
@@ -1,19 +1,16 @@
 class Solution {
     public boolean areSimilar(int[][] mat, int k) {
         int m = mat.length, n = mat[0].length;
-
         k %= n;
+
+        if (k == 0) {
+            return true;
+        }
 
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                if (i % 2 == 0) {
-                    if (mat[i][j] != mat[i][(j + k) % n]) {
-                        return false;
-                    }
-                } else {
-                    if (mat[i][j] != mat[i][(j - k + n) % n]) {
-                        return false;
-                    }
+                if (mat[i][j] != mat[i][(j - k + n) % n]) {
+                    return false;
                 }
             }
         }

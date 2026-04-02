f874fe4036820dc07e380137e5218d77eb443791
KyungMin_Kim <107176570+rvbear@users.noreply.github.com>
2026-04-02 21:17:44 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 3418-maximum-amount-of-money-robot-can-earn/3418-maximum-amount-of-money-robot-can-earn.java
+++ 3418-maximum-amount-of-money-robot-can-earn/3418-maximum-amount-of-money-robot-can-earn.java
@@ -1,41 +1,34 @@
 class Solution {
     public int maximumAmount(int[][] coins) {
-        int m = coins.length, n = coins[0].length;
-        int[][][] dp = new int[m][n][3];
+        int n = coins[0].length;
+        int[][] dp = new int[n + 1][3];
 
-        for (int[][] row : dp) {
-            for (int[] col : row) {
-                Arrays.fill(col, (int) -1e9);
+        for (int i = 0; i <= n; i++) {
+            Arrays.fill(dp[i], Integer.MIN_VALUE / 2);
+        }
+
+        for (int i = 0; i < 3; i++) {
+            dp[1][i] = 0;
+        }
+
+        for (int[] row : coins) {
+            for (int j = 1; j <= n; j++) {
+                int x = row[j - 1];
+
+                dp[j][2] = Math.max(
+                    Math.max(dp[j - 1][2] + x, dp[j][2] + x),
+                    Math.max(dp[j - 1][1], dp[j][1])
+                );
+
+                dp[j][1] = Math.max(
+                    Math.max(dp[j - 1][1] + x, dp[j][1] + x),
+                    Math.max(dp[j - 1][0], dp[j][0])
+                );
+                
+                dp[j][0] = Math.max(dp[j - 1][0], dp[j][0]) + x;
             }
         }
 
-        dp[0][0][1] = 0;
-        dp[0][0][2] = 0;
-        dp[0][0][0] = coins[0][0];
-
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                for (int k = 0; k < 3; k++) {
-                    if (i > 0) {
-                        dp[i][j][k] = Math.max(dp[i][j][k], dp[i - 1][j][k] + coins[i][j]);
-                    }
-
-                    if (i > 0 && k > 0) {
-                        dp[i][j][k] = Math.max(dp[i][j][k], dp[i - 1][j][k - 1]);
-
-                    }
-
-                    if (j > 0) {
-                        dp[i][j][k] = Math.max(dp[i][j][k], dp[i][j - 1][k] + coins[i][j]);
-                    }
-
-                    if (j > 0 && k > 0) {
-                        dp[i][j][k] = Math.max(dp[i][j][k], dp[i][j - 1][k - 1]);
-                    }
-                }
-            }
-        }
-
-        return Math.max(dp[m - 1][n - 1][0], Math.max(dp[m - 1][n - 1][1], dp[m - 1][n - 1][2]));
+        return dp[n][2];
     }
 }

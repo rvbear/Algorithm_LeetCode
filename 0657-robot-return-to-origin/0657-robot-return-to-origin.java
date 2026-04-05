02cb83a1abfce60f112228a50c39c74c201fbadc
KyungMin_Kim <107176570+rvbear@users.noreply.github.com>
2026-04-05 21:54:32 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0657-robot-return-to-origin/0657-robot-return-to-origin.java
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.java
@@ -1,19 +1,11 @@
 class Solution {
     public boolean judgeCircle(String moves) {
-        int leftCount = 0, upCount = 0;
-
+        int[] val = new int[26];
+        
         for (int i = 0; i < moves.length(); i++) {
-            if (moves.charAt(i) == 'L') {
-                leftCount++;
-            } else if (moves.charAt(i) == 'R') {
-                leftCount--;
-            } else if (moves.charAt(i) == 'U') {
-                upCount++;
-            } else if (moves.charAt(i) == 'D') {
-                upCount--;
-            }
+            val[moves.charAt(i) - 'A']++;
         }
 
-        return leftCount == 0 && upCount == 0;
+        return val['L' - 'A'] == val['R' - 'A'] && val['U' - 'A'] == val['D' - 'A'];
     }
 }

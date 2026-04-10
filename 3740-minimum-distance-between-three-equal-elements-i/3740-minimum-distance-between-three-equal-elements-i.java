82c8c4288371744eae91f49cd1630f5d6fed6eca
KyungMin_Kim <107176570+rvbear@users.noreply.github.com>
2026-04-10 22:19:04 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 3740-minimum-distance-between-three-equal-elements-i/3740-minimum-distance-between-three-equal-elements-i.java
+++ 3740-minimum-distance-between-three-equal-elements-i/3740-minimum-distance-between-three-equal-elements-i.java
@@ -1,21 +1,24 @@
 class Solution {
     public int minimumDistance(int[] nums) {
-        if (nums.length < 3) {
+        int n = nums.length, min = Integer.MAX_VALUE;
+
+        if (n < 3) {
             return -1;
         }
 
-        int min = Integer.MAX_VALUE;
-
-        for (int i = 0; i < nums.length - 2; i++) {
-            for (int j = i + 1; j < nums.length - 1; j++) {
-                for (int k = j + 1; k < nums.length; k++) {
-                    if (nums[i] == nums[j] && nums[j] == nums[k]) {
-                        min = Math.min(min, Math.abs(i - j) + Math.abs(j - k) + Math.abs(k - i));
+        for (int i = 0; i < n - 2; i++) {
+            for (int j = i + 1; j < n - 1; j++) {
+                if (nums[i] == nums[j]) {
+                    for (int k = j + 1; k < n; k++) {
+                        if (nums[j] == nums[k]) {
+                            min = Math.min(min, k - i);
+                            break;
+                        }
                     }
                 }
             }
         }
 
-        return min == Integer.MAX_VALUE ? -1 : min;
+        return min == Integer.MAX_VALUE ? -1 : min * 2;
     }
 }

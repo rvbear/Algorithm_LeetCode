2c98eb4431004ad7cd9298e67eef9c480fdca90c
KyungMin_Kim <107176570+rvbear@users.noreply.github.com>
2026-04-29 22:07:13 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 3225-maximum-score-from-grid-operations/3225-maximum-score-from-grid-operations.java
+++ 3225-maximum-score-from-grid-operations/3225-maximum-score-from-grid-operations.java
@@ -1,50 +1,33 @@
 class Solution {
-    public long maximumScore(int[][] grid) {
-        int n = grid.length;
-        long[][] arr = new long[n][n + 1];
+    private long score(int[][] grid, long[] dp, int col, long prev, int row, int dir, int stop) {
+		long max = 0;
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                arr[i][j + 1] = arr[i][j] + grid[j][i];
-            }
-        }
+		while (row != stop) {
+            max = Math.max(max, prev);
+			prev = dp[row];
+            max += grid[row][col];
+			dp[row] = max;
+            row += dir;
+		}
 
-        long[] pdp = new long[n + 1];
-        long[] pep = new long[n + 1];
+		return max;
+	}
 
-        for (int i = 1; i < n; i++) {
-            long[] dp = new long[n + 1];
-            long[] ep = new long[n + 1];
+	public long maximumScore(int[][] grid) {
+		int n = grid.length;
+		long[] dp1 = new long[n];
+		long[] dp2 = new long[n];
+		long result = 0, prev1 = 0, prev2 = 0;
+        int i = 0;
 
-            for (int pb = 0; pb <= n; pb++) {
-                for (int cb = 0; cb <= n; cb++) {
-                    long pv = 0;
+		while (i < n - 1) {
+			long curr = score(grid, dp1, i, prev1, 0, 1, n);
+			prev1 = Math.max(result, prev2);
+			prev2 = score(grid, dp2, i + 1, result, n - 1, -1, -1);
+		    result = Math.max(prev1, curr);
+            i++;
+		}
 
-                    if (cb > pb) {
-                        pv = arr[i - 1][cb] - arr[i - 1][pb];
-                    }
-
-                    long cv = 0;
-
-                    if (cb < pb) {
-                        cv = arr[i][pb] - arr[i][cb];
-                    }
-
-                    dp[cb] = Math.max(dp[cb], Math.max(pv + pdp[pb], pep[pb]));
-                    ep[cb] = Math.max(ep[cb], Math.max(cv + pep[pb], cv + pv + pdp[pb]));
-                }
-            }
-
-            pdp = dp;
-            pep = ep;
-        }
-
-        long answer = 0;
-
-        for (long v : pep) {
-            answer = Math.max(answer, v);
-        }
-
-        return answer;
-    }
+		return Math.max(result, prev2);
+	}
 }

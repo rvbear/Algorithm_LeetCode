e3c10ad88e30afd67cb307a39d539f67561940ea
KyungMin_Kim <107176570+rvbear@users.noreply.github.com>
2026-05-08 23:13:44 +0900

    Time: 463 ms (39.73%), Space: 203.1 MB (68.04%) - LeetHub

---
+++ 3629-minimum-jumps-to-reach-end-via-prime-teleportation/3629-minimum-jumps-to-reach-end-via-prime-teleportation.java
@@ -0,0 +1,69 @@
+class Solution {
+    public int minJumps(int[] nums) {
+        int n = nums.length;
+        Map<Integer, List<Integer>> port = new HashMap<>();
+
+        for (int i = 0; i < n; i++) {
+            int temp = nums[i];
+
+            for (int d = 2; d * d <= temp; d++) {
+                if (temp % d == 0) {
+                    port.computeIfAbsent(d, k -> new ArrayList<>()).add(i);
+
+                    while (temp % d == 0) {
+                        temp /= d;
+                    }
+                }
+            }
+
+            if (temp > 1) {
+                port.computeIfAbsent(temp, k -> new ArrayList<>()).add(i);
+            }
+        }
+
+        boolean[] visited = new boolean[n];
+        Queue<Integer> q = new LinkedList<>();
+        q.offer(0);
+        int steps = 0;
+
+        while (!q.isEmpty()) {
+            int sz = q.size();
+
+            while (sz-- > 0) {
+                int i = q.poll();
+
+                if (visited[i]) {
+                    continue;
+                }
+
+                visited[i] = true;
+
+                if (i == n - 1) {
+                    return steps;
+                }
+
+                int num = nums[i];
+
+                if (port.containsKey(num)) {
+                    for (int nei : port.get(num)) {
+                        q.offer(nei);
+                    }
+
+                    port.remove(num);
+                }
+
+                if (i + 1 < n) {
+                    q.offer(i + 1);
+                }
+
+                if (i - 1 >= 0) {
+                    q.offer(i - 1);
+                }
+            }
+
+            steps++;
+        }
+
+        return steps;
+    }
+}

a93877970335b2caf221be7a70ac3065afc1a27d
KyungMin_Kim <107176570+rvbear@users.noreply.github.com>
2026-05-24 20:53:31 +0900

    Time: 5 ms (100%), Space: 46.1 MB (88.29%) - LeetHub

--- 1340-jump-game-v/1340-jump-game-v.java
+++ 1340-jump-game-v/1340-jump-game-v.java
@@ -1,41 +1,52 @@
 class Solution {
-    int n;
-    int[] dp;
-
-    private int dfs(int i, int[] arr, int d) {
-        if (dp[i] != 0) {
-            return dp[i];
+    private int dfs(int i, int[] left, int[] right, int[] memo) {
+        if (i < 0) {
+            return 0;
         }
 
-        int best = 1;
-
-        for (int next = i + 1; next <= Math.min(n - 1, i + d); next++) {
-            if (arr[next] >= arr[i]) {
-                break;
-            }
-
-            best = Math.max(best, 1 + dfs(next, arr, d));
+        if (memo[i] == 0) {
+            memo[i] = Math.max(dfs(left[i], left, right, memo), dfs(right[i], left, right, memo)) + 1;
         }
 
-        for (int next = i - 1; next >= Math.max(0, i - d); next--) {
-            if (arr[next] >= arr[i]) {
-                break;
-            }
-
-            best = Math.max(best, 1 + dfs(next, arr, d));
-        }
-
-        return dp[i] = best;
+        return memo[i];
     }
 
     public int maxJumps(int[] arr, int d) {
-        n = arr.length;
-        dp = new int[n];
-
-        int answer = 1;
+        int n = arr.length;
+        int[] left = new int[n];
+        int[] st = new int[n];
+        int top = -1;
 
         for (int i = 0; i < n; i++) {
-            answer = Math.max(answer, dfs(i, arr, d));
+            int x = arr[i];
+
+            while (top >= 0 && arr[st[top]] <= x) {
+                top--;
+            }
+
+            left[i] = top < 0 || i - st[top] > d ? -1 : st[top];
+            st[++top] = i;
+        }
+
+        int[] right = new int[n];
+        top = -1;
+
+        for (int i = n - 1; i >= 0; i--) {
+            int x = arr[i];
+
+            while (top >= 0 && arr[st[top]] <= x) {
+                top--;
+            }
+
+            right[i] = top < 0 || st[top] - i > d ? -1 : st[top];
+            st[++top] = i;
+        }
+
+        int[] memo = new int[n];
+        int answer = 0;
+
+        for (int i = 0; i < n; i++) {
+            answer = Math.max(answer, dfs(i, left, right, memo));
         }
 
         return answer;

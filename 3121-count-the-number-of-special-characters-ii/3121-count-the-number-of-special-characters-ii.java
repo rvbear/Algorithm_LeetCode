409ca8d5d780214d71d754829bb09912e01966ad
KyungMin_Kim <107176570+rvbear@users.noreply.github.com>
2026-05-27 21:37:38 +0900

    Time: 8 ms (97.11%), Space: 48.1 MB (64.88%) - LeetHub

--- 3121-count-the-number-of-special-characters-ii/3121-count-the-number-of-special-characters-ii.java
+++ 3121-count-the-number-of-special-characters-ii/3121-count-the-number-of-special-characters-ii.java
@@ -1,36 +1,25 @@
 class Solution {
     public int numberOfSpecialChars(String word) {
-        int[] lastLow = new int[26];
-        int[] firstUp = new int[26];
+        int[] lastLower = new int[26];
+        int[] firstUpper = new int[26];
 
-        Arrays.fill(lastLow, -1);
-        Arrays.fill(firstUp, -1);
-
-        Set<Integer> invalid = new HashSet<>();
+        Arrays.fill(lastLower, -1);
+        Arrays.fill(firstUpper, -1);
 
         for (int i = 0; i < word.length(); i++) {
             char c = word.charAt(i);
 
-            if (c >= 'a' && c <= 'z') {
-                int index = c - 'a';
-                lastLow[index] = i;
-
-                if (firstUp[index] != -1) {
-                    invalid.add(index);
-                }
-            } else {
-                int index = c - 'A';
-
-                if (firstUp[index] == -1) {
-                    firstUp[index] = i;
-                }
+            if (c >= 'a') {
+                lastLower[c - 'a'] = i;
+            } else if (firstUpper[c - 'A'] == -1) {
+                firstUpper[c - 'A'] = i;
             }
         }
 
         int answer = 0;
 
         for (int i = 0; i < 26; i++) {
-            if (lastLow[i] != -1 && firstUp[i] != -1 && !invalid.contains(i)) {
+            if (lastLower[i] != -1 && lastLower[i] < firstUpper[i]) {
                 answer++;
             }
         }

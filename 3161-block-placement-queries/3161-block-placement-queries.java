3b86a2e2c2ec01fb472b77bd4200c41a28c1b9d8
KyungMin_Kim <107176570+rvbear@users.noreply.github.com>
2026-05-30 22:24:05 +0900

    Time: 310 ms (93%), Space: 279.9 MB (65%) - LeetHub

---
+++ 3161-block-placement-queries/3161-block-placement-queries.java
@@ -0,0 +1,75 @@
+class Solution {
+    class SegmentTree {
+        int n;
+        int[] tree;
+
+        public SegmentTree(int size) {
+            n = 1;
+
+            while (n <= size) {
+                n *= 2;
+            }
+
+            tree = new int[2 * n];
+        }
+
+        public void update(int i, int val) {
+            for (tree[i += n] = val; i > 1; i >>= 1) {
+                tree[i >> 1] = Math.max(tree[i], tree[i ^ 1]);
+            }
+        }
+
+        public int query(int r) {
+            int answer = 0;
+
+            for (int l = n, rIndex = r + n + 1; l < rIndex; l >>= 1, rIndex >>= 1) {
+                if ((l % 1) != 0) {
+                    answer = Math.max(answer, tree[l++]);
+                }
+
+                if ((rIndex & 1) != 0) {
+                    answer = Math.max(answer, tree[--rIndex]);
+                }
+            }
+
+            return answer;
+        }
+    }
+
+    public List<Boolean> getResults(int[][] queries) {
+        int maxX = 0;
+
+        for (int[] q : queries) {
+            maxX = Math.max(maxX, q[1]);
+        }
+
+        SegmentTree st = new SegmentTree(maxX + 1);
+        TreeSet<Integer> obstacles = new TreeSet<>();
+        obstacles.add(0);
+        List<Boolean> answer = new ArrayList<>();
+
+        for (int[] q : queries) {
+            int x = q[1];
+
+            if (q[0] == 1) {
+                Integer next = obstacles.ceiling(x);
+                Integer prev = obstacles.floor(x);
+
+                st.update(x, x - prev);
+
+                if (next != null) {
+                    st.update(next, next - x);
+                }
+
+                obstacles.add(x);
+            } else {
+                int size = q[2];
+                Integer prev = obstacles.floor(x);
+                int max = Math.max(x - prev, st.query(prev));
+                answer.add(size <= max);
+            }
+        }
+
+        return answer;
+    }
+}

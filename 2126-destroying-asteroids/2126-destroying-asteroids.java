e2d4ff62d6c5ffa682f3dbc753f85360891882fa
KyungMin_Kim <107176570+rvbear@users.noreply.github.com>
2026-05-31 21:41:00 +0900

    Time: 4 ms (100%), Space: 105.5 MB (96.77%) - LeetHub

--- 2126-destroying-asteroids/2126-destroying-asteroids.java
+++ 2126-destroying-asteroids/2126-destroying-asteroids.java
@@ -1,16 +1,38 @@
 class Solution {
     public boolean asteroidsDestroyed(int mass, int[] asteroids) {
-        Arrays.sort(asteroids);
-        long currentMass = mass;
+        int min = 100000;
+        int max = 0;
 
-        for (int asteroid : asteroids) {
-            if (currentMass < asteroid) {
+        for (int x : asteroids) {
+            max = Math.max(x, max);
+            min = Math.min(x, min);
+        }
+
+        int[] freq = new int[max + 1];
+        long val = mass;
+
+        for (int x : asteroids) {
+            if (x > val) {
+                freq[x]++;
+            } else {
+                val += x;
+            }
+        }
+
+        if (val >= max) {
+            return true;
+        }
+
+        for (int i = min; i <= max; i++) {
+            if (val < i) {
                 return false;
             }
 
-            currentMass += asteroid;
+            if (freq[i] != 0) {
+                val += i * freq[i];
+            }
         }
-
+        
         return true;
     }
 }

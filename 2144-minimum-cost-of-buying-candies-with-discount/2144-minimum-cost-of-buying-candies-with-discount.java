943bef58ed4dd3162eb4bba0dc89e967e33755cd
KyungMin_Kim <107176570+rvbear@users.noreply.github.com>
2026-06-01 21:10:29 +0900

    Time: 1 ms (100%), Space: 44 MB (97.34%) - LeetHub

--- 2144-minimum-cost-of-buying-candies-with-discount/2144-minimum-cost-of-buying-candies-with-discount.java
+++ 2144-minimum-cost-of-buying-candies-with-discount/2144-minimum-cost-of-buying-candies-with-discount.java
@@ -1,20 +1,27 @@
 class Solution {
-    public int minimumCost(int[] cost) {
-        cost = Arrays.stream(cost)
-                    .boxed()
-                    .sorted(Collections.reverseOrder())
-                    .mapToInt(Integer::intValue)
-                    .toArray();
-        int answer = 0, index = 0;
+    public int minimumCost(int[] costs) {
+        int[] candies = new int[101];
 
-        for (int c : cost) {
-            if (index % 3 != 2) {
-                answer += c;
-            }
-
-            index++;
+        for (int c : costs) {
+            candies[c]++;
         }
 
-        return answer;
+        int bought = 0;
+        int cost = 0;
+        
+        for (int i = 100; i > 0; i--) {
+            if (candies[i] > 2 - bought) {
+                candies[i] -= (3 - bought);
+                cost += (2 - bought) * i;
+                cost += 2 * i * (candies[i] / 3);
+                bought = candies[i] % 3;
+                cost += i * bought;
+            } else {
+                bought += candies[i];
+                cost += i * candies[i];
+            }
+        }
+        
+        return cost;
     }
 }

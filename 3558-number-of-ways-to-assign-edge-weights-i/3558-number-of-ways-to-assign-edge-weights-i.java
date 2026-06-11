6002f1dc4f2a7567853eff3a000ebbc633a8a229
KyungMin_Kim <107176570+rvbear@users.noreply.github.com>
2026-06-11 23:45:52 +0900

    Time: 27 ms (100%), Space: 265.2 MB (80.95%) - LeetHub

--- 3558-number-of-ways-to-assign-edge-weights-i/3558-number-of-ways-to-assign-edge-weights-i.java
+++ 3558-number-of-ways-to-assign-edge-weights-i/3558-number-of-ways-to-assign-edge-weights-i.java
@@ -1,28 +1,16 @@
 class Solution {
-    private static final int MOD = 1_000_000_007;
+    static final long MOD = 1_000_000_007L;
 
-    private int dfs(int node, int prev, List<Integer>[] graph) {
-        int dist = 0;
-
-        for (int g : graph[node]) {
-            if (g != prev) {
-                dist = Math.max(dist, dfs(g, node, graph) + 1);
-            }
-        }
-
-        return dist;
-    }
-
-    private long pow(long base, int exp) {
+    private long modPow(long a, int b) {
         long answer = 1;
 
-        while (exp > 0) {
-            if (exp % 2 > 0) {
-                answer = answer * base % MOD;
+        while (b > 0) {
+            if ((b & 1) == 1) {
+                answer = answer * a % MOD;
             }
 
-            base = base * base % MOD;
-            exp /= 2;
+            a = a * a % MOD;
+            b >>= 1;
         }
 
         return answer;
@@ -30,14 +18,51 @@ class Solution {
 
     public int assignEdgeWeights(int[][] edges) {
         int n = edges.length + 1;
-        List<Integer>[] graph = new ArrayList[n + 1];
-        Arrays.setAll(graph, i -> new ArrayList<>());
+
+        int[] deg = new int[n + 1];
 
         for (int[] e : edges) {
-            graph[e[0]].add(e[1]);
-            graph[e[1]].add(e[0]);
+            deg[e[0]]++;
+            deg[e[1]]++;
         }
 
-        return (int) pow(2, dfs(1, 0, graph) - 1);
+        int[][] g = new int[n + 1][];
+
+        for (int i = 1; i <= n; i++) {
+            g[i] = new int[deg[i]];
+        }
+
+        int[] idx = new int[n + 1];
+
+        for (int[] e : edges) {
+            int u = e[0], v = e[1];
+            g[u][idx[u]++] = v;
+            g[v][idx[v]++] = u;
+        }
+
+        int[] depth = new int[n + 1];
+        boolean[] vis = new boolean[n + 1];
+
+        int[] q = new int[n];
+        int l = 0, r = 0;
+        q[r++] = 1;
+        vis[1] = true;
+
+        int maxDepth = 0;
+
+        while (l < r) {
+            int u = q[l++];
+            maxDepth = Math.max(maxDepth, depth[u]);
+
+            for (int v : g[u]) {
+                if (!vis[v]) {
+                    vis[v] = true;
+                    depth[v] = depth[u] + 1;
+                    q[r++] = v;
+                }
+            }
+        }
+
+        return (int) modPow(2, maxDepth - 1);
     }
 }

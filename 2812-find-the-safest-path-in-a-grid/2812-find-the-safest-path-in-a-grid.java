274b5b19c60f66ed9b37653ffb2678f86c1c5bb9
KyungMin_Kim <107176570+rvbear@users.noreply.github.com>
2026-07-01 15:47:03 +0900

    Time: 88 ms (97.78%), Space: 114 MB (92.7%) - LeetHub

--- 2812-find-the-safest-path-in-a-grid/2812-find-the-safest-path-in-a-grid.java
+++ 2812-find-the-safest-path-in-a-grid/2812-find-the-safest-path-in-a-grid.java
@@ -1,65 +1,73 @@
 class Solution {
-    private static final int[][] dirs = {{-1, 0}, {1, 0}, {0, 1}, {0, -1}};
+    private static int[][] dir = {{-1, 0}, {1, 0}, {0, 1}, {0, -1}};
 
-    public int maximumSafenessFactor(List<List<Integer>> grid) {
+    public static int maximumSafenessFactor(List<List<Integer>> grid) {
         int n = grid.size();
-
+        
         if (grid.get(0).get(0) == 1 || grid.get(n - 1).get(n - 1) == 1) {
             return 0;
         }
 
+        int[][] dist = new int[n][n];
         Queue<int[]> q = new LinkedList<>();
+        
+        for (int i = 0; i < n; i++) {
+            Arrays.fill(dist[i], -1);
+        }
 
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 if (grid.get(i).get(j) == 1) {
                     q.add(new int[] {i, j});
+                    dist[i][j] = 0;
                 }
             }
         }
 
-        while (q.size() > 0) {
-            int[] head = q.poll();
-            int i = head[0];
-            int j = head[1];
-            int v = grid.get(i).get(j);
+        while (!q.isEmpty()) {
+            int[] pop = q.poll();
+            int r = pop[0], c = pop[1];
 
-            for (int[] d : dirs) {
-                int x = i + d[0];
-                int y = j + d[1];
+            for (int d[] : dir) {
+                int nr = d[0] + r;
+                int nc = d[1] + c;
 
-                if (Math.min(x, y) >= 0 && Math.max(x, y) < n && grid.get(x).get(y) == 0) {
-                    grid.get(x).set(y, v + 1);
-                    q.add(new int[] {x, y});
+                if (nr >= 0 && nr < n && nc >= 0 && nc < n && dist[nr][nc] == -1) {
+                    dist[nr][nc] = dist[r][c] + 1;
+                    q.add(new int[]{nr, nc});
                 }
             }
+
         }
 
         PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> b[0] - a[0]);
+        boolean[][] vis = new boolean[n][n];
 
-        pq.add(new int[] {grid.get(0).get(0), 0, 0});
+        pq.add(new int[] {dist[0][0], 0, 0});
+        vis[0][0] = true;
 
-        while (pq.size() > 0) {
-            int[] head = pq.poll();
-            int sf = head[0];
-            int i = head[1];
-            int j = head[2];
+        while (!pq.isEmpty()) {
+            int[] pop = pq.poll();
+            int safenessFactor = pop[0];
+            int r = pop[1], c = pop[2];
 
-            if (i == n - 1 && j == n - 1) {
-                return sf - 1;
+            if (r == n - 1 && c == n - 1) {
+                return safenessFactor;
             }
 
-            for (int[] d : dirs) {
-                int x = i + d[0];
-                int y = j + d[1];
+            for (int d[] : dir) {
+                int nr = r + d[0];
+                int nc = c + d[1];
 
-                if (Math.min(x, y) >= 0 && Math.max(x, y) < n && grid.get(x).get(y) > 0) {
-                    pq.add(new int[] {Math.min(sf, grid.get(x).get(y)), x, y});
-                    grid.get(x).set(y, grid.get(x).get(y) * -1);
+                if (nr >= 0 && nr < n && nc >= 0 && nc < n && !vis[nr][nc]) {
+                    int newSafeness = Math.min(safenessFactor, dist[nr][nc]);
+
+                    pq.add(new int[] {newSafeness, nr, nc});
+                    vis[nr][nc] = true;
                 }
             }
         }
 
-        return grid.get(n - 1).get(n - 1) - 1;
+        return 0;
     }
 }

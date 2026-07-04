482ea84ab9a3baae65f73b08d3d20c881b7b8620
KyungMin_Kim <107176570+rvbear@users.noreply.github.com>
2026-07-04 22:15:39 +0900

    Time: 6 ms (100%), Space: 127.9 MB (95.61%) - LeetHub

--- 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.java
+++ 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.java
@@ -1,31 +1,41 @@
 class Solution {
-    private int find(int[] root, int i) {
-        if (root[i] == i) {
-            return i;
+    private int[] parent;
+    private int[] minEdge;
+
+    private int find(int x) {
+        while (parent[x] != x) {
+            parent[x] = parent[parent[x]];
+            x = parent[x];
         }
 
-        return root[i] = find(root, root[i]);
+        return x;
+    }
+
+    private void union(int a, int b, int d) {
+        int parentA = find(a);
+        int parentB = find(b);
+
+        if (parentA != parentB) {
+            parent[parentA] = parentB;
+            minEdge[parentB] = Math.min(minEdge[parentB], Math.min(minEdge[parentA], d));
+        } else {
+            minEdge[parentA] = Math.min(minEdge[parentA], d);
+        }
     }
 
     public int minScore(int n, int[][] roads) {
-        int[] root = new int[n + 1];
-        
+        parent = new int[n + 1];
+        minEdge = new int[n + 1];
+
         for (int i = 0; i <= n; i++) {
-            root[i] = i;
+            parent[i] = i;
+            minEdge[i] = Integer.MAX_VALUE;
         }
 
-        for (int[] r : roads) {
-            root[find(root, r[0])] = find(root, r[1]);
+        for (int[] road : roads) {
+            union(road[0], road[1], road[2]);
         }
 
-        int answer = 10001;
-
-        for (int[] r : roads) {
-            if (find(root, r[0]) == find(root, 1)) {
-                answer = Math.min(answer, r[2]);
-            }
-        }
-
-        return answer;
+        return minEdge[find(1)];
     }
 }

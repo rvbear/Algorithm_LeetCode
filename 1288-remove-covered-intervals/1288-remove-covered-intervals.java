734cbea59992e0cb632c9e986028748a54143864
KyungMin_Kim <107176570+rvbear@users.noreply.github.com>
2026-07-06 22:35:42 +0900

    Time: 3 ms (100%), Space: 46.3 MB (82.87%) - LeetHub

--- 1288-remove-covered-intervals/1288-remove-covered-intervals.java
+++ 1288-remove-covered-intervals/1288-remove-covered-intervals.java
@@ -1,19 +1,32 @@
 class Solution {
     public int removeCoveredIntervals(int[][] intervals) {
-        Arrays.sort(intervals, (a, b) -> a[0] != b[0] ? a[0] - b[0] : b[1] - a[1]);
+        int n = intervals.length;
+        int remaining = n;
 
-        int n = intervals.length, count = 0;
-        int low = intervals[0][0], high = intervals[0][1];
+        for (int i = 0; i < n; i++) {
+            boolean covered = false;
+            int a = intervals[i][0];
+            int b = intervals[i][1];
 
-        for (int i = 1; i < n; i++) {
-            if (intervals[i][0] == low || intervals[i][1] <= high) {
-                count++;
-            } else {
-                low = intervals[i][0];
-                high = intervals[i][1];
+            for (int j = 0; j < n; j++) {
+                if (i == j) {
+                    continue;
+                }
+
+                int c = intervals[j][0];
+                int d = intervals[j][1];
+
+                if (c <= a && b <= d) {
+                    covered = true;
+                    break;
+                }
+            }
+
+            if (covered) {
+                remaining--;
             }
         }
-
-        return n - count;
+        
+        return remaining;
     }
 }

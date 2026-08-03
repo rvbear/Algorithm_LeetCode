a882e8b943c424ce2d4e63aa122a05d5b83386e3
KyungMin_Kim <107176570+rvbear@users.noreply.github.com>
2026-08-03 23:37:25 +0900

    Time: 6 ms (92.61%), Space: 85.5 MB (71.59%) - LeetHub

---
+++ 1406-stone-game-iii/1406-stone-game-iii.java
@@ -0,0 +1,30 @@
+class Solution {
+    public String stoneGameIII(int[] stoneValue) {
+        int n = stoneValue.length;
+        int[] dp = new int[n + 1];
+
+        for (int i = n - 1; i >= 0; i--) {
+            int best = Integer.MIN_VALUE;
+            int runningSum = 0;
+
+            for (int j = 1; j <= 3; j++) {
+                if (i + j > n) {
+                    break;
+                }
+
+                runningSum += stoneValue[i + j - 1];
+                best = Math.max(best, runningSum - dp[i + j]);
+            }
+
+            dp[i] = best;
+        }
+
+        if (dp[0] > 0) {
+            return "Alice";
+        } else if (dp[0] < 0) {
+            return "Bob";
+        } else {
+            return "Tie";
+        }
+    }
+}

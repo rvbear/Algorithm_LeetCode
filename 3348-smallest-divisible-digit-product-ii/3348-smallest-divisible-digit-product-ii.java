8b573019abe1065ad287298180c3bbfdec6594d0
KyungMin_Kim <107176570+rvbear@users.noreply.github.com>
2026-08-07 23:27:03 +0900

    Time: 23 ms (57.14%), Space: 49.9 MB (51.43%) - LeetHub

---
+++ 3348-smallest-divisible-digit-product-ii/3348-smallest-divisible-digit-product-ii.java
@@ -0,0 +1,83 @@
+class Solution {
+    public String smallestNumber(String num, long t) {
+        long temp = t;
+
+        for (int i = 2; i <= 9; i++) {
+            while (temp % i == 0) {
+                temp /= i;
+            }
+        }
+
+        if (temp > 1) {
+            return "-1";
+        }
+
+        int n = num.length();
+        int pos = n - 1;
+        char[] numChars = num.toCharArray();
+        long[] rem = new long[n + 1];
+
+        rem[0] = t;
+
+        for (int i = 0; i < n; i++) {
+            if (numChars[i] == '0') {
+                pos = i;
+                break;
+            }
+
+            rem[i + 1] = rem[i] / gcd(rem[i], numChars[i] - '0');
+        }
+
+        if (rem[n] == 1) {
+            return num;
+        }
+
+        for (int i = pos; i >= 0; i--) {
+            while (++numChars[i] <= '9') {
+                long tNow = rem[i] / gcd(rem[i], numChars[i] - '0');
+                int k = 9;
+
+                for (int j = n - 1; j > i; j--) {
+                    while (tNow % k != 0) {
+                        k--;
+                    }
+
+                    tNow /= k;
+                    numChars[j] = (char) ('0' + k);
+                }
+
+                if (tNow == 1) {
+                    return new String(numChars);
+                }
+            }
+        }
+
+        StringBuilder answer = new StringBuilder();
+        long originalT = t;
+
+        for (int i = 9; i > 1; i--) {
+            while (originalT % i == 0) {
+                answer.append((char) ('0' + i));
+                originalT /= i;
+            }
+        }
+
+        int padding = Math.max(n + 1 - answer.length(), 0);
+
+        for (int i = 0; i < padding; i++) {
+            answer.append('1');
+        }
+
+        return answer.reverse().toString();
+    }
+
+    private long gcd(long a, long b) {
+        while (b != 0) {
+            long temp = b;
+            b = a % b;
+            a = temp;
+        }
+
+        return a;
+    }
+}
